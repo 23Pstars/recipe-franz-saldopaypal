1dd4540f5bf5d31fd4256bba8bd7ad33a6919c12
23Pstars <23Pstars@gmail.com>
2019-05-30 10:04:39 +0800

    updates

--- webview.js
+++ webview.js
@@ -5,6 +5,7 @@ const path = require('path');
 module.exports = Franz => {
     Franz.injectCSS(path.join(__dirname, 'style.css'));
     Franz.loop(() => {
-        Franz.setBadge(document.getElementById('_orders').getAttribute('_pending'));
-    })
+        Franz.setBadge(parseInt(
+            document.getElementById('_orders').getAttribute('_pending')));
+    });
 }
